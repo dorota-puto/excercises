e2af1a5043ae2260104666ce2fd44fe8fc7ad507
Dorota Puto <dootras@gmail.com>
2019-09-24 01:30:37 +0200

    Generate company structure

---
+++ src/main/java/puto/lab/CompanyGenerator.java
@@ -0,0 +1,54 @@
+package puto.lab;
+
+import java.util.Random;
+
+public class CompanyGenerator {
+    public String generateName() {
+        String[] names = {"Abraham", "William", "George", "Mike", "Rose", "Kate"};
+        String[] surnames = {"Linkoln", "Barrows", "Scofield", "Kowalski", "Moniuszko"};
+        int nameIndex = new Random().nextInt(names.length);
+        int surnameIndex = new Random().nextInt(surnames.length);
+        return names[nameIndex] + " " + surnames[surnameIndex];
+    }
+
+    public String generateRole() {
+        String[] roles = {"Junior", "Regular", "Regular II", "Associate", "Lead", "Senior", "Senior II"};
+        int roleIndex = new Random().nextInt(roles.length);
+        return roles[roleIndex];
+    }
+
+    public Employee generateWorker() {
+        boolean generateDeveloper = new Random().nextBoolean();
+        if (generateDeveloper) {
+            return new Developer(generateName(), generateRole());
+        }
+        return new Tester(generateName(), generateRole());
+    }
+
+    public Manager generateManager() {
+        return new GroupManager(generateName(), generateRole(), new Random().nextInt(4) + 1);
+    }
+
+    public void hireEmployees(Manager manager, int h) {
+        if (h <= 1) {
+            manager.hire(generateWorker());
+        }
+        while (manager.canHire()) {
+            boolean hireManager = new Random().nextBoolean();
+            if (hireManager) {
+                Manager managerToHire = generateManager();
+                manager.hire(managerToHire);
+                hireEmployees(managerToHire,h-1);
+            }else{
+                manager.hire(generateWorker());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CompanyGenerator companyGenerator = new CompanyGenerator();
+        Manager ceo = companyGenerator.generateManager();
+        companyGenerator.hireEmployees(ceo,4);
+        System.out.println(ceo.work());
+    }
+}
