8231d9845858838f934af168258ab87804448fc2
Dorota Puto <dootras@gmail.com>
2019-10-15 00:38:54 +0200

    sorting flats by owners implemented

--- src/main/java/puto/airbnb/service/AirbnbFlatService.java
+++ src/main/java/puto/airbnb/service/AirbnbFlatService.java
@@ -1,13 +1,56 @@
 package puto.airbnb.service;
 
+import org.apache.commons.lang3.builder.CompareToBuilder;
 import puto.airbnb.model.AirbnbFlat;
+import puto.collection.model.Animal;
 
-import java.util.List;
+import java.util.*;
 
 public class AirbnbFlatService {
-    public static void main (String[] args){
-        AirbnbFlatDataReader reader= new AirbnbFlatDataReader();
-       List<AirbnbFlat> result= reader.read("C:\\workspace\\excercises\\src\\main\\resources\\airbnb\\AB_NYC_2019.csv");
+    public Map<Integer, Integer> getNumberOfFlatsByOwners(List<AirbnbFlat> flats) {
+        Map<Integer, Integer> numberOfFlatsByOwners = new HashMap<>();
+        for (AirbnbFlat flat : flats) {
+            if (!numberOfFlatsByOwners.containsKey(flat.getHostId())) {
+                numberOfFlatsByOwners.put(flat.getHostId(), 1);
+            } else {
+                Integer previousNumber = numberOfFlatsByOwners.get(flat.getHostId());
+                numberOfFlatsByOwners.put(flat.getHostId(), previousNumber + 1);
+            }
 
+        }
+        return numberOfFlatsByOwners;
+    }
+
+    public Map<Integer, List<AirbnbFlat>> getFlatbyOwner(List<AirbnbFlat> flats) {
+        Map<Integer, List<AirbnbFlat>> listOfFlatsByOwner = new HashMap<>();
+        for (AirbnbFlat flat : flats) {
+            if (!listOfFlatsByOwner.containsKey(flat.getHostId())) {
+                List<AirbnbFlat> ownerFlats = new ArrayList<>();
+                ownerFlats.add(flat);
+                listOfFlatsByOwner.put(flat.getHostId(), ownerFlats);
+            } else {
+                listOfFlatsByOwner.get(flat.getHostId()).add(flat);
+            }
+        }
+        return listOfFlatsByOwner;
+    }
+
+    public Map<Integer, List<AirbnbFlat>> getTopFlatOwner(List<AirbnbFlat> flats, int n) {
+        Map<Integer, List<AirbnbFlat>> listOfFlatsByTopOwner = new HashMap<>();
+        Map<Integer, List<AirbnbFlat>> listOfFlatsByOwners = getFlatbyOwner(flats);
+        List<Map.Entry<Integer, List<AirbnbFlat>>> pairs = new ArrayList<>(listOfFlatsByOwners.entrySet());
+
+        pairs.sort(new Comparator<Map.Entry<Integer, List<AirbnbFlat>>>() {
+            @Override
+            public int compare(Map.Entry<Integer, List<AirbnbFlat>> o1, Map.Entry<Integer, List<AirbnbFlat>> o2) {
+                return new CompareToBuilder()
+                        .append(o2.getValue().size(), o1.getValue().size())
+                        .toComparison();
+            }
+        });
+        for (int i=0; i<n; i++){
+            listOfFlatsByTopOwner.put(pairs.get(i).getKey(),pairs.get(i).getValue());
+        }
+        return listOfFlatsByTopOwner;
     }
 }
---
+++ src/main/java/puto/airbnb/service/AirbnbnFlatServicePlayground.java
@@ -0,0 +1,32 @@
+package puto.airbnb.service;
+
+import puto.airbnb.model.AirbnbFlat;
+
+import java.util.List;
+import java.util.Map;
+
+public class AirbnbnFlatServicePlayground {
+    public static void main(String[] args) {
+        AirbnbFlatDataReader reader = new AirbnbFlatDataReader();
+        List<AirbnbFlat> result = reader.read("C:\\workspace\\excercises\\src\\main\\resources\\airbnb\\AB_NYC_2019.csv");
+
+        AirbnbFlatService flatService = new AirbnbFlatService();
+        //   Map<Integer, Integer> numberOfFlatsByOwners = flatService.getNumberOfFlatsByOwners(result);
+
+        //   for (Integer ownerId : numberOfFlatsByOwners.keySet()) {
+        //      System.out.println("owner id = " + ownerId + " has " + numberOfFlatsByOwners.get(ownerId)+" flats");
+        //   }
+
+        Map<Integer, List<AirbnbFlat>> listOfFlatsByOwners = flatService.getTopFlatOwner(result,1);
+
+        for (Integer ownerId : listOfFlatsByOwners.keySet()) {
+            String name = listOfFlatsByOwners.get(ownerId).get(0).getHostName();
+            Integer numberOfFlats = listOfFlatsByOwners.get(ownerId).size();
+            System.out.println("owner id = " + ownerId + " name = " + name + " has " + numberOfFlats + " flats: ");
+            for (AirbnbFlat flat : listOfFlatsByOwners.get(ownerId)) {
+                System.out.println("    " + flat.getId() + " name = " + flat.getFlatName());
+            }
+
+        }
+    }
+}
